7cae715fe075da3ef9a43f556062dc1aaaab0a8e
Eric Zhou <ericzhou1212@gmail.com>
2023-10-24 20:18:51 -0700

    finished

--- exp_demo2.js
+++ exp_demo2.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-
+app.get('/hello',function(req, res) {
+    res.send("Hello world");
+});
 
 
 app.listen(3000);
--- exp_demo3b.js
+++ exp_demo3b.js
@@ -5,7 +5,8 @@ var app = express();
 
 //both exp-demo3a.js and exp-demo3b.js are in same directory
 
-
+var routedemo = require('./exp-demo3a.js');
+app.use('/routedemo',routedemo);
 app.listen(3000);
 
 
